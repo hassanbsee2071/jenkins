137fdf0b05e93580a59e4007b9956ef99fc461af
hassanbsee2071 <hassan.bsee2071@iiu.edu.pk>
2020-06-29 01:04:42 +0500

    Third Version

--- javascriptANDjquery/c01/js/add-content.js
+++ javascriptANDjquery/c01/js/add-content.js
@@ -3,13 +3,13 @@ var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18) {
-    greeting = 'Good evening!';
+    greeting = 'Good evening! ALLAH O AKBAR';
 } else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
+    greeting = 'Good afternoon!Love ALLAH';
 } else if (hourNow > 0) {
-    greeting = 'Good morning!';
+    greeting = 'Good morning!Love Prophet(P.B.U.H)';
 } else {
-    greeting = 'Welcome!';
+    greeting = 'Welcome!To Islam';
 }
 
 document.write('<h3>' + greeting + '</h3>');
